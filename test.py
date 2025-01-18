4215aa1ff98c298735278263a5ade2405159fee6
IRFAN S <123147439+irfanbroo@users.noreply.github.com>
2025-01-18 12:56:04 +0530

    Capturing 10 packets from my wifi

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 import pyshark
+
+# Capture live packets on the specified interface
+
+capture = pyshark.LiveCapture(interface='Wi-Fi')
+
+# Start the capture process
+capture.sniff(packet_count=10)
+
+# Print captured packets
+for packet in capture:
+    print(packet)
